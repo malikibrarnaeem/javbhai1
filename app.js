e845973ef5963c56791fd31fbc276e550c0f0d4d
malikibrarnaeem <95858450+malikibrarnaeem@users.noreply.github.com>
2021-12-09 16:54:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,192 @@
+// Chapteer # 1
+// alert("Hello")
+// alert("welcome to JS land")
+// console.log("How i am?")
+// Chapteer # 1 end
+
+
+// Chapteer # 2
+
+
+// var name = ("ibrar malik")
+// var bio = ("19 years old")
+// var designations = ("wed developer")
+// alert(( name)
+// alert(mybio)
+// alert(designations)
+// var ab = ["pizza","pizz","piz","pi","p"]
+// alert(ab)
+// var mail = ("email123@gmail.com")
+// alert(mail)
+// var ba = ("a smarter way to learn java script")
+// alert(ba)
+// var document = ("Yah! i can write HTML content through  java script")
+// document.write(document)
+// var ab = ("_______________________"/#**#/"____________________")
+// alert(a)
+
+
+// chapter # 2 end
+
+// chapter # 3 start
+
+// var userage = ("I am 18 years old")
+// alert(userage)
+// var ab = ("you have visited 2 times this times")
+// alert(ab)
+// var ba = ("My birth year is 2002")
+// var ab = ("data type of this varible is number")
+// document.write(ba)
+// document.write(ab)
+// var ca = ("John Doe ordered-5 T-shirt(s) on XYZ Clothing store")
+// document.write(ca)
+
+
+// chapter # 3 end
+
+// chapter # 4 start
+
+// var firs = ("Rules for naming JS variables”")
+// var firsvaR = ("Variable names can only contain")
+// var FirsvariBle = (" Variables must begin with a")
+// var se__condvar = ("Variable names are case")
+// var $econdvariBle = ("Variable names should not be JS")
+
+// document.write(firs)
+
+// document.write(firsvaR)
+// document.write(FirsvariBle)
+// document.write(se__condvar)
+// document.write($econdvariBle)
+
+// chapter # 4 end
+
+
+// chapter # 5 start
+
+// var ab = 4;
+// var ba = 5;
+
+// alert(ab+ba)
+
+// var ab = 3;
+// var ba = 2;
+
+// alert(ab*ba)
+// var ab = 6;
+// var ba = 6;
+
+// alert(ab/ba)
+
+// var ab = 11;
+// var ba = 5;
+
+// alert(ab-ba)
+
+
+// var ab = 3;
+// for(var i =1; i <=10; i++){
+    //     document.write(ab *i)
+    // }
+    
+    
+    // var ab = 650;
+    // var ba = +prompt("how many items you want?")
+    // var ca = 100;
+    // var da = +prompt("how many items you want?")
+    // var total = (ab*ba) + (ca*da)
+    // document.write(total)
+    
+    
+    // var ab = 1000;
+    // var ba = +prompt("how many numbers you get?")
+    // var total = (ba/1000*100)
+    // alert(total)
+    
+    // var usdollar = 104;
+    // var pakistanRS = +prompt("how many dollar u have?")
+    // var prs = (usdollar*pakistanRS)
+    // alert(prs)
+    
+    // chapter # 5 end
+
+
+    // chapter6 to 9 start
+    
+    // var ab = 3;
+    // var ba = a++;
+    // alert(ab)
+    
+    // var ab = 2;
+    // var ba = a--;
+    // alert(ab)
+    
+    
+    
+    // var ab = 2;
+    // var ba = ++a;
+    // alert(ab)
+    
+    // var ab = 2;
+    // var ba = 1;
+    
+    // var c   = --ab - --ba + ++ba + ba--;
+    
+    // document.write(c)
+    // console.log("value of ab=1 (value of ba=10)")
+
+
+    // var eng = +prompt("english marks 100 (enter your marks)")
+    // var urd = +prompt("urdu marks 100(enter your marks)")
+    // var sci = +prompt("science marks 100(enter your marks)")
+    // var total = 300;
+    // var username = (eng+urd+sci)
+    // alert(username/total*100)
+    
+    // chapter 6 to 9 end
+    
+    
+    // var ab = ("karachi")
+    // var ba = ("isl")
+    // var ca = ("lahore")
+    // var usergiven = prompt("enter your city")
+    
+    
+    // if(usergiven == ab){
+    //     alert("welcome the city of light")
+    // }
+    // else alert ("sorry you are not allowed")
+    
+//     var ab = "red"
+//     var ba = "yellow"
+//     var ca = "green"
+    
+//     var usercolor = prompt("enter color")
+
+//     if(usercolor == ab){
+//         alert("Must stop")
+//     }
+//     if(usercolor == ba){
+//         alert("ready to move")
+//     }
+//     if(usercolor == ca){
+//         alert("move now")
+//     }
+//     else {
+//         alert("sorry this is not valid")
+// }
+ 
+
+
+    
+    
+
+
+
+
+    
+
+    
+    
+
+
